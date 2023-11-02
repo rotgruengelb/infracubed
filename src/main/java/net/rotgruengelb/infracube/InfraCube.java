804ce918d1e098fa1fce483e768a277984c61a0f
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2023-11-02 09:14:13 +0100

    Update InfraCube.java

--- src/main/java/net/rotgruengelb/infracube/InfraCube.java
+++ src/main/java/net/rotgruengelb/infracube/InfraCube.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public class InfraCube implements ModInitializer {
 
-    public static final String MOD_ID = "infracube";
+    public static final String MOD_ID = "infracubed";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
